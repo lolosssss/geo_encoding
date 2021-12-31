9771163e569b4ba5d71bb7f3d16fae08f2f88c74
Evan <awesomeyggd@gmail.com>
2021-12-31 17:29:45 +0800

    Add openstreetmap

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,53 @@
-<script setup lang="ts">
-import { ref } from 'vue';
+<script lang="ts">
+import { ref, defineComponent } from 'vue';
+import { Deck } from '@deck.gl/core';
+import { BitmapLayer } from '@deck.gl/layers';
+import { S2Layer, TileLayer } from '@deck.gl/geo-layers';
 
-const count = ref(0);
+export default {
+  setup() {
+    return {
+      deck: null,
+      viewState: ref({
+        latitude: 30.551537058389985,
+        longitude: 119.89694203247012,
+        zoom: 12,
+        bearing: 0,
+        pitch: 0,
+      }),
+    };
+  },
+  mounted() {
+    const tilelayer = new TileLayer({
+      data: 'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      minZoom: 0,
+      maxZoom: 26,
+      tileSize: 256,
+      renderSubLayers: (props) => {
+        const {
+          bbox: { west, south, east, north },
+        } = props.tile;
+        return new BitmapLayer(props, {
+          data: null,
+          image: props.data,
+          bounds: [west, south, east, north],
+        });
+      },
+    });
+    this.deck = new Deck({
+      canvas: 'map-container',
+      width: '100%',
+      height: '100%',
+      initialViewState: this.viewState,
+      controller: true,
+      layers: [tilelayer],
+    });
+  },
+};
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Under construction</h1>
+  <canvas id="map-container"></canvas>
 </template>
 
 <style scoped>
