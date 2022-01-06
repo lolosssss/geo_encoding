6718d57a2393fae01d05e0688c67566dd603d09e
Evan <awesomeyggd@gmail.com>
2022-01-06 15:39:55 +0800

    Remove deckgl-typings and update tsconfig

---
+++ src/components/ConfigView.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      selected: 0,
+    };
+  },
+  mounted() {},
+};
+</script>
+
+<template>
+  <div>
+    <div class="h-full w-full bg-white/80 flex flex-col p-8">
+      <div class="w-full flex flex-row border-2 rounded-md border-sky-500">
+        <div class="flex-1 border-r-2 border-sky-500">GeoJSON</div>
+        <div class="flex-1 border-r-2 border-sky-500">S2</div>
+        <div class="flex-1">H3</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,14 +3,16 @@ import { ref, defineComponent } from 'vue';
 import { Deck } from '@deck.gl/core';
 import { BitmapLayer } from '@deck.gl/layers';
 import { S2Layer, TileLayer } from '@deck.gl/geo-layers';
+import ConfigView from './ConfigView.vue';
 
 export default {
   setup() {
     return {
       deck: null,
       viewState: ref({
-        latitude: 30.551537058389985,
-        longitude: 119.89694203247012,
+        // 西湖区
+        longitude: 120.142116,
+        latitude: 30.245532,
         zoom: 12,
         bearing: 0,
         pitch: 0,
@@ -18,36 +20,47 @@ export default {
     };
   },
   mounted() {
-    const tilelayer = new TileLayer({
-      data: 'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      minZoom: 0,
-      maxZoom: 26,
-      tileSize: 256,
-      renderSubLayers: (props) => {
-        const {
-          bbox: { west, south, east, north },
-        } = props.tile;
-        return new BitmapLayer(props, {
-          data: null,
-          image: props.data,
-          bounds: [west, south, east, north],
-        });
-      },
-    });
-    this.deck = new Deck({
-      canvas: 'map-container',
-      width: '100%',
-      height: '100%',
-      initialViewState: this.viewState,
-      controller: true,
-      layers: [tilelayer],
-    });
+    this.initMap();
+  },
+  methods: {
+    initMap() {
+      const tilelayer = new TileLayer({
+        data: 'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        minZoom: 0,
+        maxZoom: 26,
+        tileSize: 256,
+        renderSubLayers: (props) => {
+          const {
+            bbox: { west, south, east, north },
+          } = props.tile;
+          return new BitmapLayer(props, {
+            data: null,
+            image: props.data,
+            bounds: [west, south, east, north],
+          });
+        },
+      });
+      this.deck = new Deck({
+        canvas: 'map-container',
+        width: '100%',
+        height: '100%',
+        initialViewState: this.viewState,
+        controller: true,
+        layers: [tilelayer],
+      });
+    },
+  },
+  components: {
+    ConfigView,
   },
 };
 </script>
 
 <template>
-  <canvas id="map-container"></canvas>
+  <div>
+    <canvas id="map-container" class="fixed inset-0"></canvas>
+    <ConfigView class="h-full w-1/5 absolute top-0 right-0" />
+  </div>
 </template>
 
 <style scoped>
